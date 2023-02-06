ebb17fbc39a7a78f1f10116c820671dd1e9d5892
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2023-02-06 15:34:48 +0200

    primitive data types and Explicit conversion

--- js_prim_types.js
+++ js_prim_types.js
@@ -1,2 +1,29 @@
-let a=5;
-console.log (a);
+// 1. Addition of different types 
+console.log ("'a'+true = ",'a'+true);
+console.log ("'a'+5 = ",'a'+5);
+console.log ("2+'a'+true = ",2+'a'+true);
+console.log ("5+'b' = ",5+'b');
+console.log ("5+1+'b' = ",5+1+'b');
+console.log ("5+true = ",5+true);
+console.log ("false+2 = ",false+2);
+// 2. Multiplication of different types 
+console.log ("'a'*true = ",'a'*true);
+console.log ("'a'*5 = ",'a'*5);
+console.log ("5*'b' = ",5*'b');
+console.log ("5*2*'b' = ",5*2*'b');
+console.log ("5*true = ",5*true);
+console.log ("false*2 = ",false*2);
+// 3. Division of different types 
+console.log ("'a'/true = ",'a'/true);
+console.log ("'a'/5 = ",'a'/5);
+console.log ("5/'b' = ",5/'b');
+console.log ("5/2/'b' = ",5/2/'b');
+console.log ("5/true = ",5/true);
+console.log ("false/2 = ",false/2);
+// 4. Explicit conversion
+console.log("Number('abc'),Number(true),Number(false), Number('23'), Number ('4abc'), Number(' 6')=");
+console.log (Number('abc'),Number(true),Number(false), Number('23'), Number ('4abc'), Number(' 6'));
+console.log("String(5),String(true), String(false), String(3.14))=");
+console.log (String(5),String(true), String(false), String(3.14));
+console.log("Boolean(5),Boolean(0),Boolean(1), Boolean(''),Boolean('abc'), Boolean(' '),Boolean(' y.6')=")
+console.log (Boolean(5),Boolean(0),Boolean(1), Boolean(''),Boolean('abc'), Boolean(' '),Boolean(' y.6'));
